ceac62fcf0c4ede05e27599406290c67ec915394
Quentin Hermiteau <qhermiteau@reply.com>
2026-05-20 15:32:35 +0200

    correction exo 11

--- src/app/exercices/11/page.jsx
+++ src/app/exercices/11/page.jsx
@@ -1,16 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { useReducer } from "react";
 import Slider from "./Slider";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+const initialState = {
+  value: 0,
+  step: 1,
+};
 
-  const handleIncrement = () => setCount(count + step);
-  const handleDecrement = () => setCount(count - step);
-  const handleReset = () => setCount(0);
-  const handleUpdateStep = (step) => setStep(step);
+const reducer = (state, { action, payload }) => {
+  switch (action) {
+    case "increment":
+      return { ...state, value: state.value + state.step };
+    case "decrement":
+      return { ...state, value: state.value - state.step };
+    case "reset":
+      return initialState;
+    case "updateStep":
+      return { ...state, step: payload };
+  }
+};
+
+export default function Counter() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  const handleIncrement = () => dispatch({ action: "increment" });
+  const handleDecrement = () => dispatch({ action: "decrement" });
+  const handleReset = () => dispatch({ action: "reset" });
+  const handleUpdateStep = (step) =>
+    dispatch({ action: "updateStep", payload: step });
 
   return (
     <main>
@@ -18,13 +36,13 @@ export default function Counter() {
         À l'aide de useReducer et setInterval, faire en sorte que le compteur
         s'incrémente automatiquement toutes les secondes.
       </div>
-      <h1>{count}</h1>
+      <h1>{count.value}</h1>
       <div>
         <button onClick={handleDecrement}>-</button>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleReset}>0</button>
       </div>
-      <Slider min={1} max={10} onChange={handleUpdateStep} />
+      <Slider min={1} max={10} value={count.step} onChange={handleUpdateStep} />
     </main>
   );
 }
